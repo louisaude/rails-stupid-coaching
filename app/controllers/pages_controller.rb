d20833280a6ac9a7cbe8a0f3ef1d01d1172998e6
Louis Aude <louis.aude@live.fr>
2020-11-09 14:27:08 +0100

    will style later if I have time

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def ask
+  end
+  def answer
+    @question = params[:question]
+    case @question
+    when 'I am going to work'
+      @answer = "Great"
+    when @question.end_with?('?')
+       @answer ='Silly question, get dressed and go to work!'
+    else
+       @answer = "I don't care, get dressed and go to work!"
+   end
+  end
+end
